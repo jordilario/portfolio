e7e5fc3446c75be9478f6554428370f364e1167a
Jordi <jordi_lario@hotmail.com>
2024-05-21 20:54:24 +0200

    primera version

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://jordilario.github.io',
+  base: 'my-repo',
 });
