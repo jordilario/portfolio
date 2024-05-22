16aac1422c4b5bb474b5f8795f3a6d862b0f3f4e
Jordi <jordi_lario@hotmail.com>
2024-05-22 18:59:41 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://jordilario.github.io',
-  base: 'my-repo',
+  integrations: [tailwind()]
 });
